c8cd0e214690ff3c26e0f91abe521cc5ae66a285
bill <foooeadonly@gmail.com>
2022-05-01 19:52:11 +0700

    🎸 Accoustix

---
+++ commands/💰 Premium/defaultautoplay.js
@@ -0,0 +1,39 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+const config = require(`${process.cwd()}/botconfig/config.json`);
+const ee = require(`${process.cwd()}/botconfig/embed.json`);
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+module.exports = {
+  name: "defaultautoplay",
+  category: "💰 Premium",
+  aliases: ["default-autoplay", "defaultap", "default-ap"],
+  cooldown: 10,
+  usage: "defaultautoplay",
+  description: "Toggles if it Autoplay should be enabled on default or not! [Default: true]",
+  memberpermissions: ["ADMINISTRATOR"],
+  type: "bot",
+  run: async (client, message, args, cmduser, text, prefix, player, es, ls) => {
+    //change the default autoplay to "true"
+    client.settings.set(message.guild.id, !client.settings.get(message.guild.id, "defaultap"), "defaultap");
+
+    return message.reply({
+      embeds: [new MessageEmbed()
+        .setFooter(client.getFooter(es))
+        .setColor(es.color)
+        .setTitle(eval(client.la[ls]["cmds"]["settings"]["defaultautoplay"]["variable1"]))
+        .setDescription(eval(client.la[ls]["cmds"]["settings"]["defaultautoplay"]["variable2"]))
+      ]
+    });
+  }
+}
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by nqbill | https://discord.gg/homely
+ * @INFO
+ * Work for Homely Development | https://homely.dev
+ * @INFO
+ * Please mention him / Homely Development, when using this Code!
+ * @INFO
+ *********************************************************/
